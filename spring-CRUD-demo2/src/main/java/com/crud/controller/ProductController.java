47f08b247ea6fb25cfbc0fe59b52a4fc30bf11f7
vamsi <vamsi1922@hotmail.com>
2024-08-10 13:35:11 +0530

    spring-CRUD-demo2 project >> added api's (update,delete) modifed code in (controller/service) layers

--- spring-CRUD-demo2/src/main/java/com/crud/controller/ProductController.java
+++ spring-CRUD-demo2/src/main/java/com/crud/controller/ProductController.java
@@ -1,7 +1,5 @@
 package com.crud.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,22 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.DTO.ProductDTO;
 import com.crud.DTO.ResponceProductDTO;
-import com.crud.model.Product;
 import com.crud.service.ProductService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
 public class ProductController {
 
+	// Instance of EmployeeService to handle business logic
 	@Autowired
 	private ProductService productService;
 	/*
-	 *  --->> insertion value into database
-	 */
+     * Endpoint to create a new product.
+     * @param productDTO The data transfer object containing the details of the product to be created.
+     * @return A response entity containing a success message and HTTP status code 201 (CREATED).
+     */
 	@PostMapping("/create/product")
 	public ResponseEntity<String> createProduct(@RequestBody ProductDTO productDTO){
 		productService.createProduct(productDTO);
@@ -33,19 +35,44 @@ public class ProductController {
 	}
 	
 	/*
-	 *   --->> fetch all data
-	 */
+     * Endpoint to fetch all products.
+     * @return A response entity containing a list of all products and HTTP status code 200 (OK).
+     */
 	@GetMapping("/fetch/product")
 	public ResponseEntity<ResponceProductDTO> getAllProduct(){
 		ResponceProductDTO responceProductDTO = productService.showAllProduct();
 		return new ResponseEntity<>(responceProductDTO,HttpStatus.OK);
 	}
 	/*
-	 *   --->> update name of the item
-	 */
-	@GetMapping("/fetch/product/{productId}/name")
+     * Endpoint to fetch a particular product by its ID.
+     * @param productId The ID of the product to be retrieved.
+     * @return A response entity containing the product details and HTTP status code 200 (OK).
+     */
+	@GetMapping("/fetch/product/{productId}")
 	public ResponseEntity<ProductDTO> getProduct(@PathVariable Integer productId){
 		ProductDTO productDTO = productService.showProduct(productId);
 		return new ResponseEntity<>(productDTO,HttpStatus.OK);
+	}
+	
+	/*
+     * Endpoint to update the name of a product by its ID.
+     * @param id The ID of the product to be updated.
+     * @param productName The new name of the product.
+     * @return A response entity containing the updated product details and HTTP status code 201 (CREATED).
+     */
+	@PutMapping("/update/product/{id}/productname")
+	public ResponseEntity<ProductDTO> updateProductNameById(@PathVariable int id, @RequestBody String productName){
+		ProductDTO productDTO = productService.updateProductName(id,productName);
+		return new ResponseEntity<>(productDTO,HttpStatus.CREATED);
+	}
+	/*
+     * Endpoint to delete a product by its ID.
+     * @param id The ID of the product to be deleted.
+     * @return A response entity containing a success message and HTTP status code 201 (CREATED).
+     */
+	@DeleteMapping("/delete/product/{id}")
+	public ResponseEntity<String> deleteProductById(@PathVariable int id){
+		productService.deleteProductByIdh(id);
+		return new ResponseEntity<>("product sucessfully",HttpStatus.CREATED);
 	}
 }
--- spring-CRUD-demo2/src/main/java/com/crud/service/ProductService.java
+++ spring-CRUD-demo2/src/main/java/com/crud/service/ProductService.java
@@ -8,10 +8,34 @@ import com.crud.model.Product;
 
 public interface ProductService {
 
+	/*
+     * Creates a new product in the database.
+     * @param productDTO The data transfer object containing the details of the product to be created.
+     * @return The created Product entity.
+     */
 	Product createProduct(ProductDTO productDTO);
-
+	 /*
+     * Retrieves all products from the database.
+     * @return A ResponceProductDTO containing a list of all ProductDTOs and the total number of products.
+     */
 	ResponceProductDTO showAllProduct();
-
+	/*
+     * Retrieves a specific product by its ID.
+     * @param productId The ID of the product to retrieve.
+     * @return A ProductDTO containing the product details, or null if the product is not found.
+     */
 	ProductDTO showProduct(Integer productId);
+	/*
+     * Updates the name of a specific product by its ID.
+     * @param id The ID of the product to update.
+     * @param productName The new name to assign to the product.
+     * @return The updated ProductDTO containing the new product details.
+     */
+	ProductDTO updateProductName(int id, String productName);
+	/*
+     * Deletes a specific product by its ID.
+     * @param id The ID of the product to delete.
+     */
+	void deleteProductByIdh(int id);
 
 }
